3215101cf0b9f6d42fee2302d0fd4c4060f0a6a0
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2022-05-13 11:43:46 +0100

    Project setup

---
+++ README.md
@@ -0,0 +1,8 @@
+#  BDG Daily Wallet
+
+A bitcoin wallet app template.
+
+### Dependencies
+- Bitcoin Development Kit
+- Lightning Development Kit
+
---
+++ dailywallet/App/DailyWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyWalletApp.swift
+//  Daily Wallet
+//
+//  Created by Daniel Nordh on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct dailywalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
