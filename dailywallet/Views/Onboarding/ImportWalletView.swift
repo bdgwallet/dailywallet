a4218e2e9819f56d2d98b9cf52ef856b8c249ee6
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2022-05-13 14:59:01 +0100

    Basic import of recovery phrase works

--- dailywallet/Views/Onboarding/ImportWalletView.swift
+++ dailywallet/Views/Onboarding/ImportWalletView.swift
@@ -6,11 +6,59 @@
 //
 
 import SwiftUI
+import BDKManager
 
 struct ImportWalletView: View {
+    @EnvironmentObject var bdkManager: BDKManager
+    @EnvironmentObject var backupManager: BackupManager
+    
+    @State private var recoveryPhrase: String = ""
+    @State private var importError = false
+    
     var body: some View {
-        Text("Import wallet")
-            .navigationTitle("Import wallet")
-            .navigationBarTitleDisplayMode(.inline)
+        VStack {
+            Spacer()
+            Text("Informational text")
+            Spacer()
+            if !importError {
+                TextField("Enter recovery phrase", text: $recoveryPhrase).padding(32)
+            } else {
+                Text("Error importing wallet")
+            }
+            Spacer()
+            if !importError {
+                VStack (spacing: 32) {
+                    Button("Import") {
+                        if !importRecoveryPhrase(recoveryPhrase: recoveryPhrase, bdkManager: bdkManager, backupManager: backupManager) {
+                            self.importError = true
+                        }
+                    }
+                    /* TODO: Advanced import settings
+                    NavigationLink(destination: AdvancedImportView()) {
+                        Text("Advanced settings")
+                    }
+                    */
+                }.padding(32)
+            }
+        }
+        .navigationTitle("Import wallet")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+func importRecoveryPhrase(recoveryPhrase: String, bdkManager: BDKManager, backupManager: BackupManager) -> Bool {
+    do {
+        // Create private key info
+        let extendedKeyInfo = try bdkManager.restoreFromMnemonic(mnemonic: recoveryPhrase, password: nil)
+        // Create descriptor and load wallet
+        let descriptor = bdkManager.createDescriptorFromXprv(descriptorType: DescriptorType.singleKey_wpkh84, xprv: extendedKeyInfo.xprv)
+        // Save backup
+        backupManager.savePrivateKey(extendedKeyInfo: extendedKeyInfo, descriptor: descriptor)
+        // Load wallet in bdkManager, this will trigger a view switch
+        bdkManager.loadWallet(descriptor: descriptor)
+        return true
+    } catch let error {
+        print(error)
+        return false
     }
 }
