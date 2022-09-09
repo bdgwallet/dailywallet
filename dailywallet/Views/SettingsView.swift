cf72ca16f21ed2d8ec99b6eeb57da579038bf2fe
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2022-09-09 17:12:51 +0100

    Dummy settings views

--- dailywallet/Views/SettingsView.swift
+++ dailywallet/Views/SettingsView.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text("Settings")
+        NavigationView {
+            List {
+                NavigationLink(destination: Text("General")) {
+                    Text("General")
+                        .padding()
+                }
+                NavigationLink(destination: Text("Fees")) {
+                    Text("Fees")
+                        .padding()
+                }
+                NavigationLink(destination: Text("Privacy")) {
+                    Text("Privacy")
+                        .padding()
+                }
+                NavigationLink(destination: Text("Security")) {
+                    Text("Security")
+                        .padding()
+                }
+                NavigationLink(destination: Text("Wallet backup")) {
+                    Text("Wallet backup")
+                        .padding()
+                }
+                NavigationLink(destination: Text("Network")) {
+                    Text("Network")
+                        .padding()
+                }
+                NavigationLink(destination: Text("Help & Support")) {
+                    Text("Help & Support")
+                        .padding()
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("Settings")
+            .navigationBarTitleDisplayMode(.inline)
+        }.accentColor(.black)
     }
 }
 
