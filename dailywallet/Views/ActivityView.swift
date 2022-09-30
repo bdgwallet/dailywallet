703737ebbb453b933a91af5ae15dda96f7f0a6d9
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2022-09-30 16:58:39 +0100

    Activity view work, list view

--- dailywallet/Views/ActivityView.swift
+++ dailywallet/Views/ActivityView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import BDKManager
 import WalletUI
+import BitcoinDevKit
 
 struct ActivityView: View {
     @EnvironmentObject var bdkManager: BDKManager
@@ -15,33 +16,28 @@ struct ActivityView: View {
     @State private var navigateTo: NavigateTo? = NavigateTo.none
     
     let headerWidth = UIScreen.main.bounds.width
-    let headerHeight = UIScreen.main.bounds.height / 4
+    let headerHeight = 200.0 //UIScreen.main.bounds.height / 5
+    var customTransactions: [CustomTransaction] = []
     
     var body: some View {
         VStack() {
             VStack(alignment: .center) {
-                Spacer()
-                Text("Your balance")
-                switch bdkManager.syncState {
-                case .synced:
-                    Text("\(bdkManager.balance) sats")
-                case .syncing:
-                    Text("Syncing")
-                default:
-                    Text("Not synced")
+                VStack(spacing: 4) {
+                    Text("Your balance")
+                        .textStyle(BitcoinBody4())
+                    Text("25,000 sats")
+                        .textStyle(BitcoinTitle1())
+                    Text("$11.48")
+                        .textStyle(BitcoinBody4())
+                }.padding(EdgeInsets(top: 0, leading: 0, bottom: 32, trailing: 0))
+                HStack {
+                    Text("Activity")
+                        .textStyle(BitcoinTitle5())
+                    Spacer()
                 }
-                Text("$11.48")
-                Spacer()
-            }.frame(width: headerWidth, height: headerHeight)
-                .background(Color.bitcoinOrange)
-            NavigationView {
-                List {
-                    Text("Hello World")
-                    Text("Hello World")
-                    Text("Hello World")
-                }
-                .navigationTitle("Menu")
-            }
+            }.frame(alignment: .bottom)
+                .padding(EdgeInsets(top: 32, leading: 16, bottom: 0, trailing: 16))
+            TransactionsView(customTransactions: customTransactions)
         }
     }
 }
@@ -51,3 +47,34 @@ struct TransactionHistory_Previews: PreviewProvider {
         ActivityView()
     }
 }
+
+struct TransactionsView: View {
+    var customTransactions: [CustomTransaction]
+    
+    var body: some View {
+        if customTransactions.count != 0 {
+            List {
+                ForEach(customTransactions) {_ in
+                    Text("Transaction")
+                        .textStyle(BitcoinBody3())
+                }
+            }.listStyle(.plain)
+        } else {
+            Spacer()
+            Text("No transactions")
+                .textStyle(BitcoinBody4())
+            Spacer()
+        }
+    }
+}
+
+// Transaction from BitcoinDevKit does not work in List, either use custom struct or wait for it to be changed. Here's a dummy:
+
+public struct CustomTransaction {
+    
+    public let id = UUID()
+    let details: TransactionDetails?
+    let confirmation: BlockTime?
+}
+
+extension CustomTransaction: Equatable, Hashable, Identifiable {}
