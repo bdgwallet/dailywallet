728b6445ed863e34449a6077da7b15a7cfafe81f
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2022-10-07 11:54:05 +0100

    Basic Request view with QR code and share / copy buttons

--- dailywallet/Views/Payments/RequestView.swift
+++ dailywallet/Views/Payments/RequestView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import BDKManager
 import WalletUI
 import BitcoinDevKit
+import CoreImage.CIFilterBuiltins
 
 struct RequestView: View {
     @EnvironmentObject var bdkManager: BDKManager
@@ -16,15 +17,23 @@ struct RequestView: View {
     @State private var requestAddress: String?
     
     var body: some View {
-        VStack {
-            Spacer()
-            Text("Request View")
-            Spacer()
-            Text(requestAddress ?? "No address")
-            Spacer()
-        }.task {
-            getAddress()
-        }
+        NavigationView {
+            VStack {
+                Spacer()
+                QRView(paymentRequest: requestAddress ?? "No address")
+                Spacer()
+                VStack {
+                    BitcoinShareButton(title: "Share", shareItem: requestAddress ?? "No address")
+                    Button("Copy") {
+                        UIPasteboard.general.string = requestAddress ?? "No address"
+                    }
+                    .buttonStyle(BitcoinPlain(width: 150))
+                }.padding(16)
+            }
+            .navigationTitle("Payment request")
+            .navigationBarTitleDisplayMode(.inline)
+        }.accentColor(.black)
+        .onAppear(perform: getAddress)
     }
     
     func getAddress() {
@@ -33,17 +42,68 @@ struct RequestView: View {
                 do {
                     let addressInfo = try bdkManager.wallet!.getAddress(addressIndex: AddressIndex.new)
                     requestAddress = addressInfo.address
-                    print(requestAddress)
+                    print(requestAddress ?? "no address")
                 } catch (let error){
                     print(error)
                 }
-                default: do {}
-            }
+            default: do {}
         }
+    }
 }
 
 struct RequestView_Previews: PreviewProvider {
     static var previews: some View {
         RequestView()
+    }
+}
+
+struct QRView: View {
+    var paymentRequest: String
+    var width = 250.0
+    var height = 250.0
+    
+    var body: some View {
+        Image(uiImage: generateQRCode(from: "bitcoin:\(paymentRequest)"))
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .frame(width: width, height: height)
+    }
+}
+
+func generateQRCode(from string: String) -> UIImage {
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    let data = Data(string.utf8)
+    filter.setValue(data, forKey: "inputMessage")
+
+    if let outputImage = filter.outputImage {
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            return UIImage(cgImage: cgimg)
+        }
+    }
+    return UIImage(systemName: "xmark.circle") ?? UIImage()
+}
+
+struct BitcoinShareButton: View {
+    var title: String
+    var shareItem: String
+    var width = 315.0
+    var cornerRadius = 8.0
+    var outlineColor = Color.bitcoinOrange
+    var lineWidth = 1.5
+    
+    var body: some View {
+        ShareLink(item: shareItem) {
+            Label(title, systemImage: "square.and.arrow.up")
+                .tint(Color.bitcoinOrange)
+                .font(Font.body.bold())
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: cornerRadius)
+                        .stroke(outlineColor, lineWidth: lineWidth)
+                        .frame(width: width)
+                )
+        }
     }
 }
