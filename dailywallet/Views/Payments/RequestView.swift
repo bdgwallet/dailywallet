9fbc6ee518855e3207bd2192f223714e6ae3b8b2
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2022-10-07 10:42:58 +0100

    Basic get address for request view

--- dailywallet/Views/Payments/RequestView.swift
+++ dailywallet/Views/Payments/RequestView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 import BDKManager
 import WalletUI
+import BitcoinDevKit
 
 struct RequestView: View {
     @EnvironmentObject var bdkManager: BDKManager
     
+    @State private var requestAddress: String?
+    
     var body: some View {
-        Text("Request View")
+        VStack {
+            Spacer()
+            Text("Request View")
+            Spacer()
+            Text(requestAddress ?? "No address")
+            Spacer()
+        }.task {
+            getAddress()
+        }
     }
+    
+    func getAddress() {
+        switch bdkManager.walletState {
+            case .loaded:
+                do {
+                    let addressInfo = try bdkManager.wallet!.getAddress(addressIndex: AddressIndex.new)
+                    requestAddress = addressInfo.address
+                    print(requestAddress)
+                } catch (let error){
+                    print(error)
+                }
+                default: do {}
+            }
+        }
 }
 
 struct RequestView_Previews: PreviewProvider {
