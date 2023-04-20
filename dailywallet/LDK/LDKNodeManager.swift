badb1d195758cc403cccf62e033be0e7cb8329b5
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2023-04-20 18:01:17 +0100

    make Node published, move build to start()

--- dailywallet/LDK/LDKNodeManager.swift
+++ dailywallet/LDK/LDKNodeManager.swift
@@ -11,24 +11,20 @@ import LightningDevKitNode
 public class LDKNodeManager: ObservableObject {
     // Public variables
     public var network: Network
-    public let node: Node
+    @Published public var node: Node?
     @Published public var onchainBalance: UInt64?
     //@Published public var transactions: [TransactionDetails] = []
     @Published public var syncState = SyncState.empty
-
+    
     // Private variables
-    //private let bdkQueue = DispatchQueue (label: "bdkQueue", qos: .userInitiated)
-    //private let databaseConfig = DatabaseConfig.memory // set DatabaseConfig.memory or .sqlite
-    //private let blockchainConfig: BlockchainConfig
+    var esploraServerUrl = ESPLORA_URL_TESTNET
+    var listeningAddress: String? = nil
+    let defaultCltvExpiryDelta = UInt32(2048)
+    let storageDirectoryPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.path
     
     // Initialize a LDKNodeManager instance on the specified network
     public init(network: String) {
         self.network = network
-        let storageDirectoryPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.path
-        var esploraServerUrl = "http://blockstream.info/testnet/api/"
-        var chosenNetwork = "testnet"
-        var listeningAddress: String? = nil
-        let defaultCltvExpiryDelta = UInt32(2048)
             
         switch network { // Update when Network type is enum instead of string
             case "regtest":
@@ -42,24 +38,24 @@ public class LDKNodeManager: ObservableObject {
                 esploraServerUrl = ESPLORA_URL_TESTNET
                 listeningAddress = "127.0.0.1:18333"
         }
-                
+    }
+    
+    // Start LDK Node
+    public func start() throws {
         let ldkConfig = Config(
             storageDirPath: storageDirectoryPath,
             esploraServerUrl: esploraServerUrl,
-            network: chosenNetwork,
+            network: network,
             listeningAddress: listeningAddress,
             defaultCltvExpiryDelta: defaultCltvExpiryDelta
         )
             
         let nodeBuilder = Builder.fromConfig(config: ldkConfig)
         let node = nodeBuilder.build()
-        self.node = node
-    }
-    
-    // Start LDK Node
-    public func start() async throws {
+        
         do {
             try node.start()
+            self.node = node
             debugPrint("LDKNodeManager: Started")
         } catch {
             debugPrint("LDKNodeManager: Error starting node: \(error.localizedDescription)")
@@ -68,23 +64,27 @@ public class LDKNodeManager: ObservableObject {
     
     // Sync once
     public func sync() {
-        do {
-            self.syncState = .syncing
-            try node.syncWallets()
-            self.syncState = .synced
-        } catch let error {
-            self.syncState = .failed(error)
-            debugPrint("LDKNodeManager: Error syncing wallets: \(error.localizedDescription)")
+        if self.node != nil {
+            do {
+                self.syncState = .syncing
+                try self.node!.syncWallets()
+                self.syncState = .synced
+            } catch let error {
+                self.syncState = .failed(error)
+                debugPrint("LDKNodeManager: Error syncing wallets: \(error.localizedDescription)")
+            }
         }
     }
     
     // Update .onchainBalance
     private func getOnchainBalance() {
-        do {
-            onchainBalance = try node.totalOnchainBalanceSats()
-            debugPrint("LDKNodeManager: Onchain balance: \(self.onchainBalance!)")
-        } catch let error {
-            print("LDKNodeManager: Error getting onchain balance: \(error.localizedDescription)")
+        if self.node != nil {
+            do {
+                onchainBalance = try self.node!.totalOnchainBalanceSats()
+                debugPrint("LDKNodeManager: Onchain balance: \(self.onchainBalance!)")
+            } catch let error {
+                print("LDKNodeManager: Error getting onchain balance: \(error.localizedDescription)")
+            }
         }
     }
 }
@@ -92,3 +92,4 @@ public class LDKNodeManager: ObservableObject {
 // Public API URLs
 //let ESPLORA_URL_BITCOIN = "https://blockstream.info/api/"
 //let ESPLORA_URL_TESTNET = "https://blockstream.info/testnet/api"
+
