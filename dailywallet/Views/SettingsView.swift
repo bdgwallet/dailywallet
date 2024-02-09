c1519c7bc9ff4cec850a16684f5dfe06822c54f8
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2024-02-09 11:49:50 +0000

    Basic node and channel info in settings

--- dailywallet/Views/SettingsView.swift
+++ dailywallet/Views/SettingsView.swift
@@ -7,44 +7,40 @@
 
 import SwiftUI
 import LDKNode
+import WalletUI
 
 struct SettingsView: View {
     var body: some View {
         NavigationView {
             List {
-                NavigationLink(destination: Text("General")) {
-                    Text("General")
-                        .padding()
+                NavigationLink(destination: NodeInfoView()) {
+                    Text("Node details")
                 }
-                NavigationLink(destination: SettingsLightningChannelsView()) {
-                    Text("Lightning channels")
-                        .padding()
+                NavigationLink(destination: ChannelListView()) {
+                    Text("Channels")
                 }
+                /*
                 NavigationLink(destination: Text("Fees")) {
                     Text("Fees")
-                        .padding()
                 }
                 NavigationLink(destination: Text("Privacy")) {
                     Text("Privacy")
-                        .padding()
                 }
                 NavigationLink(destination: Text("Security")) {
                     Text("Security")
-                        .padding()
                 }
                 NavigationLink(destination: Text("Wallet backup")) {
                     Text("Wallet backup")
-                        .padding()
                 }
                 NavigationLink(destination: Text("Network")) {
                     Text("Network")
-                        .padding()
                 }
                 NavigationLink(destination: Text("Help & Support")) {
                     Text("Help & Support")
-                        .padding()
                 }
+                 */
             }
+            .padding(16)
             .listStyle(.plain)
             .navigationTitle("Settings")
             .navigationBarTitleDisplayMode(.inline)
@@ -58,31 +54,98 @@ struct SettingsView_Previews: PreviewProvider {
     }
 }
 
-struct SettingsLightningChannelsView: View {
+struct NodeInfoView: View {
+    @EnvironmentObject var ldkNodeManager: LDKNodeManager
+    
+    var body: some View {
+        NavigationView {
+            List {
+                VStack(alignment: .leading) {
+                    Text("nodeId")
+                        .textStyle(BitcoinBody5()).bold()
+                    Text(ldkNodeManager.node?.nodeId() ?? "No id")
+                        .textStyle(BitcoinBody5())
+                }
+            }
+            .padding(8)
+            .listStyle(.plain)
+            .navigationTitle("Node details")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ChannelListView: View {
     @EnvironmentObject var ldkNodeManager: LDKNodeManager
     
     var ids: [String] {
-            ldkNodeManager.channels.map { $0.channelId }
-        }
+        ldkNodeManager.channels.map { $0.channelId }
+    }
     
     var body: some View {
         if ldkNodeManager.channels.count != 0 {
-                List {
-                    ForEach(ids, id: \.self) { channel in
-                        NavigationLink(destination: Text("General")) {
-                            Text(channel)
-                                .padding()
+            List {
+                ForEach(ids.indices, id: \.self) { index in
+                    let id = ids[index]
+                    NavigationLink(destination: ChannelInfoView(channelDetails: ldkNodeManager.channels.first(where: { $0.channelId == id })!)) {
+                        VStack(alignment: .leading) {
+                            Text("Channel \(index + 1)") // Generate channel number dynamically
+                                .textStyle(BitcoinBody5())
+                                .bold()
+                            Text(id)
+                                .textStyle(BitcoinBody5())
                         }
+                        .padding(8)
                     }
                 }
-                .listStyle(.plain)
-                .navigationTitle("Lightning channels")
-                .navigationBarTitleDisplayMode(.inline)
+            }
+            .listStyle(.plain)
+            .navigationTitle("Lightning channels")
+            .navigationBarTitleDisplayMode(.inline)
         } else {
             Spacer()
             Text("No transactions")
-                //.textStyle(BitcoinBody4())
             Spacer()
         }
     }
 }
+
+struct ChannelInfoView: View {
+    @EnvironmentObject var ldkNodeManager: LDKNodeManager
+    
+    var channelDetails: ChannelDetails
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(flattenChannelDetails(), id: \.label) { detail in
+                    VStack(alignment: .leading) {
+                        Text(detail.label)
+                            .textStyle(BitcoinBody5()).bold()
+                        Text(detail.value)
+                        .textStyle(BitcoinBody5())
+                    }
+                    .padding(8)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("Channel details")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .accentColor(.black)
+    }
+    
+    // Function to flatten ChannelDetails into an array of tuples (label, value)
+    func flattenChannelDetails() -> [(label: String, value: String)] {
+        var detailsArray: [(label: String, value: String)] = []
+        
+        let mirror = Mirror(reflecting: channelDetails)
+        for case let (label?, value) in mirror.children {
+            detailsArray.append((label: label, value: String(describing: value)))
+        }
+        
+        return detailsArray
+    }
+}
+
+
