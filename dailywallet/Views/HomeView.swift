7a07cf295aefb6f78ae8720b6bcd27b904bbb571
Daniel Nordh <3393669+danielnordh@users.noreply.github.com>
2024-02-16 15:49:34 +0000

    Set Payments view in the middle and default tab

--- dailywallet/Views/HomeView.swift
+++ dailywallet/Views/HomeView.swift
@@ -12,22 +12,23 @@ struct HomeView: View {
     @EnvironmentObject var ldkNodeManager: LDKNodeManager
     @EnvironmentObject var backupManager: BackupManager
     @State var blockHeight: UInt32?
+    @State private var activeTab = 2
     
     var body: some View {
-        TabView {
+        TabView(selection: $activeTab) {
+            ActivityView()
+                .tabItem {
+                    Label("Activity", systemImage: "list.bullet")
+                }.tag(1)
             PaymentsView()
                 .environmentObject(backupManager)
                 .tabItem {
                     Label("Payments", systemImage: "arrow.up.arrow.down")
-                }
-            ActivityView()
-                .tabItem {
-                    Label("Activity", systemImage: "list.bullet")
-                }
+                }.tag(2)
             SettingsView()
                 .tabItem {
                     Label("Settings", systemImage: "gearshape.fill")
-                }
+                }.tag(3)
         }.accentColor(.bitcoinOrange)
             .task {
                 //
